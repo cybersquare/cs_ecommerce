088ac8162ee5eb1588ea8d5c0c6320a5f7bd318e
cybersquare <content@baabte.com>
2021-01-26 11:16:23 +0530

    staic files dirs added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,3 +125,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)

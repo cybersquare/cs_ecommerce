9ce7417781d9cb46725464f44421a95caa3e1fb3
rushni888 <rushni@baabte.com>
2021-01-28 13:05:00 +0530

    test

--- ecom/urls.py
+++ ecom/urls.py
@@ -5,5 +5,8 @@ urlpatterns=[
     # path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path('login',views.login,name='login'),
-    path('signup',views.signup,name='signup')
+    path('signup',views.signup,name='signup'),
+    path('search_products',views.search_products,name='Search'),
+    path('view_product',views.view_product,name='view'),
+
 ]
--- ecom/views.py
+++ ecom/views.py
@@ -10,3 +10,7 @@ def login(request):
     return render(request,"ecom/login.html")
 def signup(request):
     return render(request,"ecom/signup.html")
+def search_products(request):
+    return render(request,"ecom/search_products.html")
+def view_product(request):
+    return render(request,"ecom/view_product.html")

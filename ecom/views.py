7e1caee7d7436aa9d4a7bc9205920bc1652f94b6
kiransurya032@gmail.com <kiransurya032@gmail.com>
2021-01-28 10:36:16 +0530

    footer added

--- ecom/views.py
+++ ecom/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("hello cybersquare, welcome")
+# def index(request):
+#     return HttpResponse("hello cybersquare, welcome")
 def home(request):
     return render(request,"ecom/index.html")
+def login(request):
+    return render(request,"ecom/login.html")
+def signup(request):
+    return render(request,"ecom/signup.html")

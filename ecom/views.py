43a39127a6225c3ab6de20c42e61223245930300
rushni888 <rushni@baabte.com>
2021-01-30 19:17:15 +0530

    test

--- ecom/views.py
+++ ecom/views.py
@@ -11,11 +11,9 @@ def login(request):
 
 def signup(request):
     return render(request,"ecom/signup.html")
-<<<<<<< HEAD
+
 def search_products(request):
     return render(request,"ecom/search_products.html")
 def view_product(request):
     return render(request,"ecom/view_product.html")
-=======
 
->>>>>>> 187b1d382ce95056600fd4a815927747f9e41117

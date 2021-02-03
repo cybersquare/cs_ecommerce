8443aeea2eb92513fdd36e70ea86a3eb40a1fc92
cybersquare <content@baabte.com>
2021-02-03 14:06:01 +0530

    validate added

---
+++ ecom/static/ecom/scripts/validate.js
@@ -0,0 +1,34 @@
+
+
+// Validate whether username and password is entered or not
+function validate_login(){ 
+    status = 1;
+    username = document.getElementById("username");
+    password = document.getElementById("userpassword");
+    if(username.value==""){
+        username.style.borderColor="#FF0000";
+        status = 0;
+    }
+    else{
+        username.style.borderColor="#ced4da"
+        status = 1;
+    }
+    if(password.value==""){
+        password.style.borderColor="#FF0000";
+        status = 0;
+    }
+    else{
+        password.style.borderColor="#ced4da";
+        status = 1;
+    }
+    if(status==0){
+        return false;
+    }
+
+} //End validate
+
+
+//Remove resubmit form while refresh
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }

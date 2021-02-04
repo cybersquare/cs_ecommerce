cd5b4320f39c8a34962a2c7ece9a2acfd6be89a7
cybersquare <content@baabte.com>
2021-02-04 16:13:35 +0530

    heroku config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,3 +184,5 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, '/ecomadmin/static'),
    ]
 
+
+django_heroku.settings(locals())

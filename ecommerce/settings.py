2a78476f55916378f6c2e2a826d8cce935fdf935
suryakiran111 <77670427+suryakiran111@users.noreply.github.com>
2021-02-04 16:08:00 +0530

    Updated database settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -89,19 +89,38 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'ecom',
+
+#         'USER': 'ecommerce',
+
+#         'PASSWORD': '1234',
+
+#         'HOST': '127.0.0.1',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
 DATABASES = {
 
     'default': {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'ecom',
+        'NAME': 'd4v9j0fe08jdcq',
 
-        'USER': 'ecommerce',
+        'USER': 'znlmihmfwalguo',
 
-        'PASSWORD': '1234',
+        'PASSWORD': '3631c0e52ce865d9f43962e0c7ca03e8b711cc113b962f1bc16ce63cf8e3989a',
 
-        'HOST': '127.0.0.1',
+        'HOST': 'ec2-54-156-73-147.compute-1.amazonaws.com',
 
         'PORT': '5432',
 

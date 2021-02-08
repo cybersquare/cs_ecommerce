a6d3659cdc54f4bf456744b6b0eabecea62ea246
kiransurya032@gmail.com <kiransurya032@gmail.com>
2021-02-08 15:47:04 +0530

    url attribute corrected

--- ecomadmin/urls.py
+++ ecomadmin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('managereseller', views.mngreseller, ame='managereseller'),
+    path('managereseller', views.mngreseller, name='managereseller'),
     path('addreseller', views.addreseller, name='addreseller'),
     path('deletereseller', views.deletereseller, name='deletereseller'),
     path('adminlogin', views.admlogin, name='adminlogin'),

1e2b9dfdd72982dd352849f94da9ba3a67723e1d
suryakiran111 <77670427+suryakiran111@users.noreply.github.com>
2021-07-27 15:34:07 +0530

    Updated corsheades in settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['https://cs-ecom.herokuapp.com/', 'cs-ecom.herokuapp.com', '127
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL=True
 ROOT_URLCONF = 'ecommerce.urls'
 
 TEMPLATES = [
